75d3a66b8f830d2346afdc06f96195c7c716aec9
Katsumi Kougen <lesanonyme1001@gmail.com>
2022-08-16 16:00:17 +0700

    Tweak the manual "reader" and add some memes to it

--- bmanual.py
+++ bmanual.py
@@ -13,7 +13,7 @@ def Start(w):
                 ManualText["fg"], ManualText["bg"] = f"#{choice(Hex)}{choice(Hex)}{choice(Hex)}{choice(Hex)}{choice(Hex)}{choice(Hex)}", f"#{choice(Hex)}{choice(Hex)}{choice(Hex)}{choice(Hex)}{choice(Hex)}{choice(Hex)}"
     
     p = tk.Toplevel(master=w, padx=15, pady=15)
-    p.geometry("800x600")
+    p.geometry("755x630")
     p.title("User Manual")
     p.resizable(False, False)
     
@@ -25,25 +25,36 @@ def Start(w):
     )
     UpperText.grid(row=0, column=0, sticky=tk.W)
     
+    LowerText = tk.Label(
+        master = p,
+        font = ("FOT-Rodin Pro", 10),
+        text =
+        	"    \"Gee, it sure is boring around here!\"\n"
+        	"    \"My boy, this user manual is what all true ASCII artists strive for!\"",
+    	justify = tk.LEFT,
+        pady = 2
+    )
+    LowerText.grid(row=1, column=0, sticky=tk.W)
+    
     ManualText = tk.Text(
         master = p,
-        font = ("DejaVu Sans Mono", 10),
+        font = ("DejaVu Sans Mono", 11, "bold"),
         width = 80,
         height = 25,
         wrap = tk.CHAR,
         cursor = "left_ptr"
     )
-    ManualText.grid(row=1, column=0)
+    ManualText.grid(row=2, column=0)
     ManualText.insert("1.0", open("manual.txt", "r").read())
     ManualText["state"] = tk.DISABLED
     
     ColourSchemesFrame = tk.Frame(master=p, pady=10)
-    ColourSchemesFrame.grid(row=2, column=0)
+    ColourSchemesFrame.grid(row=3, column=0)
     ColourSchemeType = tk.IntVar()
     ColourSchemeRdb = [
         tk.Radiobutton(
             master = ColourSchemesFrame,
-            font = ("FOT-Rodin Pro", 11, "bold"),
+            font = ("FOT-Rodin Pro", 11),
             text = "Black on white",
             variable = ColourSchemeType,
             value = 0,
@@ -51,7 +62,7 @@ def Start(w):
         ),
         tk.Radiobutton(
             master = ColourSchemesFrame,
-            font = ("FOT-Rodin Pro", 11, "bold"),
+            font = ("FOT-Rodin Pro", 11),
             text = "White on black",
             variable = ColourSchemeType,
             value = 1,
@@ -59,7 +70,7 @@ def Start(w):
         ),
         tk.Radiobutton(
             master = ColourSchemesFrame,
-            font = ("FOT-Rodin Pro", 11, "bold"),
+            font = ("FOT-Rodin Pro", 11),
             text = "Whatever on LSD",
             variable = ColourSchemeType,
             value = 2,
@@ -69,3 +80,11 @@ def Start(w):
     for idx, i in enumerate(ColourSchemeRdb):
         i.grid(row=0, column=idx)
     ColourSchemeRdb[0].select()
+    
+    CloseButton = tk.Button(
+        master = p,
+        font = ("FOT-Rodin Pro", 12),
+        text = "Close",
+        command = p.destroy
+    )
+    CloseButton.grid(row=4, column=0)
